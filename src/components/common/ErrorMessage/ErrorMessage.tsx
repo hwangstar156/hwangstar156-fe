26c1e72013c2f5b75a5b8535e3787f9b527d65ae
hwangjungmin <hwangstar156@gmail.com>
2022-11-13 03:31:18 +0900

    feat: add ErrorMessage Component

---
+++ src/components/common/ErrorMessage/ErrorMessage.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const ErrorMessage = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: calc(100vh - 120px);
+`;
+
+export default ErrorMessage;
