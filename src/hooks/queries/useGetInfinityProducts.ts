ee0754d5616aecf3e45e6dfd0e67af76b3c1bfde
hwangjungmin <hwangstar156@gmail.com>
2022-11-12 22:36:37 +0900

    feat: add infinityscroll with react-query useInfinityQuery

---
+++ src/hooks/queries/useGetInfinityProducts.ts
@@ -0,0 +1,39 @@
+import { useContext, useEffect } from 'react';
+import { useInfiniteQuery } from 'react-query';
+import { getInfinityProducts } from '../../api/products';
+import { InfinityProductsContext } from '../../provider/InfinityProductsProvider';
+
+const useGetInfinityProducts = ({
+  page,
+  setCurrentPage,
+}: {
+  page: number;
+  setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
+}) => {
+  const { setProducts } = useContext(InfinityProductsContext);
+  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery(
+    ['infinity-products'],
+    ({ pageParam = page }) => getInfinityProducts({ page: pageParam }),
+    {
+      getNextPageParam: ({ data, nextPage }) => {
+        const totalPageLength = Math.ceil(data.data.totalCount / 16);
+        page !== nextPage && setCurrentPage(nextPage);
+        return page > totalPageLength ? undefined : nextPage;
+      },
+      refetchOnWindowFocus: false,
+    }
+  );
+
+  useEffect(() => {
+    if (!data) {
+      return;
+    }
+
+    const products = data.pages.flatMap((page) => page.data.data.products);
+    setProducts(products);
+  }, [data]);
+
+  return { data, fetchNextPage, hasNextPage };
+};
+
+export default useGetInfinityProducts;
--- src/pages/infinite-scroll.tsx
+++ src/pages/infinite-scroll.tsx
@@ -1,16 +1,30 @@
-import Link from 'next/link';
 import type { NextPage } from 'next';
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import styled from 'styled-components';
 
 import products from '../api/data/products.json';
 import ProductList from '../components/ProductList';
+import useObserver from '../hooks/useObserver';
+import useGetInfinityProducts from '../hooks/queries/useGetInfinityProducts';
 
 const InfiniteScrollPage: NextPage = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const { fetchNextPage, hasNextPage } = useGetInfinityProducts({
+    page: currentPage,
+    setCurrentPage,
+  });
+
+  const handleIntersect = useCallback(async () => {
+    await fetchNextPage();
+  }, [fetchNextPage]);
+
+  const { handleElementRef } = useObserver({ hasNext: !!hasNextPage, callback: handleIntersect });
+
   return (
     <>
       <Container>
-        <ProductList products={products} />
+        <ProductList />
+        <div ref={handleElementRef}></div>
       </Container>
     </>
   );
